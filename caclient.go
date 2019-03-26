2726c8772b761c312a143e316330b79eeb1e1a8e
lk <lk2684753@163.com>
2019-03-26 16:51:23 +0800

    add certificate info

--- caclient.go
+++ caclient.go
@@ -22,6 +22,8 @@ package digital_identity
 
 import (
 	"crypto/x509"
+	"encoding/pem"
+	"github.com/cloudflare/cfssl/csr"
 	"net/http"
 )
 
@@ -53,4 +55,202 @@ var CA *FabricCAClient
 type ServerInfo struct {
 	CAName string
 	CACert *x509.Certificate
+}
+
+
+// RegistrationRequest holds all data needed for new registration of new user in Certificate Authority
+type CARegistrationRequest struct {
+	// EnrolmentId is unique name that identifies identity
+	EnrolmentId string `json:"id"`
+	// Type defines type of this identity (user,client, auditor etc...)
+	Type string `json:"type"`
+	// Secret is password that will be used for enrollment. If not provided random password will be generated
+	Secert string `json:"secert,omitempty" mask:"password" help:"The enrollment secret for the identity being registered"`
+	// MaxEnrollments define maximum number of times that identity can enroll. If not provided or is 0 there is no limit
+	MaxEnrollments int `json:"max_enrollments,omitempty"`
+	// Affiliation associates identity with particular organisation.
+	Affiliation string `json:"affiliation" help:"The identity's affiliation"`
+	// Attrs are attributes associated with this identity
+	Attrs []CaRegisterAttribute `json:"attrs,omitempty"`
+	// CAName is the name of the CA that should be used.
+	CAName string `json:"caname,omitempty" skip:"true"`
+}
+
+// CaRegisterAttribute holds user attribute used for registration
+// for example user may have attr `accountType` with value `premium`
+// this attributes can be accessed in chainCode and build business logic on top of them
+type CaRegisterAttribute struct {
+	// Name is the name of the attribute.
+	Name string `json:"name"`
+	// Value is the value of the attribute. Can be empty string
+	Value string `json:"value"`
+	// ECert define how this attribute will be included in ECert. If this value is true this attribute will be
+	// added to ECert automatically on Enrollment if no attributes are requested on Enrollment request.
+	ECert bool `json:"ecert,omitempty"`
+}
+
+type certificateRequest struct {
+	CaEnrollmentRequest
+	CR string `json:"certificate_request"`
+}
+
+// enrollmentResponse is response from fabric-ca server for enrolment that contains created Ecert
+type enrollmentResponse struct {
+	caResponse
+	Result enrollmentResponseResult `json:"result"`
+}
+
+type enrollmentResponseResult struct {
+	Cert       string
+	ServerInfo enrollmentResponseServerInfo
+	Version    string
+}
+
+type enrollmentResponseServerInfo struct {
+	CAName  string
+	CAChain string
+}
+
+// CAResponse represents response message from fabric-ca server
+type caResponse struct {
+	Success  bool            `json:"success"`
+	Errors   []caResponseErr `json:"errors"`
+	Messages []string        `json:"messages"`
+}
+
+type caResponseErr struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+type CaEnrollAttribute struct {
+	// Name is the name of the attribute
+	Name string `json:"name"`
+	// Optional define behaviour when required attribute is not available to user. If `true` then request will continue,
+	// but attribute will not be included in ECert. If `false` and attribute is missing, request will fail.
+	// If false and attribute is available, request will continue and attribute will be added in ECert
+	Optional bool `json:"optional,omitempty"`
+}
+
+// CaEnrollmentRequest holds data needed for getting ECert (enrollment) from CA server
+type CaEnrollmentRequest struct {
+	EnrollmentId string `json:"name" skip:"true"`
+	// Secret is the password for this identity
+	Secret string `json:"secret,omitempty" skip:"true" mask:"password"`
+	Profile string `json:"profile,omitempty"`
+	//Label is used for hardware secure modules.
+	Label string `json:"label,omitempty"`
+	CAName string `json:"caname,omitempty" skip:"true"`
+	// Host is the list of valid host names for this certificate. If empty default hosts will be used
+	Hosts []string `json:"hosts"`
+	// Attrs are the attributes that must be included in ECert. This is subset of the attributes used in registration.
+	Attrs []CaEnrollAttribute `json:"attr_reqs,omitempty"`
+	// CSR is Certificate Signing Request info
+	CSR *CSRInfo `json:"csr,omitempty" help:"Certificate Signing Request info"`
+}
+
+type CSRInfo struct {
+	CN           string           `json:"CN"`
+	Names        []csr.Name       `json:"names,omitempty"`
+	Hosts        []string         `json:"hosts,omitempty"`
+	KeyRequest   *BasicKeyRequest `json:"key,omitempty"`
+	CA           *csr.CAConfig    `json:"ca,omitempty"`
+	SerialNumber string           `json:"serial_number,omitempty"`
+}
+
+// BasicKeyRequest encapsulates size and algorithm for the key to be generated
+type BasicKeyRequest struct {
+	Algo string `json:"algo" yaml:"algo"`
+	Size int    `json:"size" yaml:"size"`
+}
+
+// CARegisterCredentialResponse credentials from palletone-ca server registration request
+type caRegisterCredentialResponse struct {
+	Secret string `json:"secret"`
+}
+
+type caRegisterResponse struct {
+	caResponse
+	Result caRegisterCredentialResponse `json:"result"`
+}
+
+// CAGetCertsResponse holds response from `GetCaCertificateChain`
+type CAGetCertResponse struct {
+	// RootCertificates is list of pem encoded certificates
+	RootCertificates []*x509.Certificate
+	// IntermediateCertificates is list of pem encoded intermediate certificates
+	IntermediateCertificates []*pem.Block
+	// CAName is the name of the CA server that returns this certificates
+	CAName string
+	// Version is the version of server that returns this certificates
+	Version string
+}
+
+type caInfoRequest struct {
+	CaName string `json:"caname,omitempty"`
+}
+
+type caInfoResponse struct {
+	caResponse
+	Result caResponseResult `json:"result"`
+}
+
+type caResponseResult struct {
+	CAName  string `json:"CAName"`
+	CAChain string `json:"CAChain"`
+	Version string `json:"Version"`
+}
+
+type CARevocationRequest struct {
+	EnrollmentId string `json:"id,omitempty"`
+	Serial string `json:"serial,omitempty"`
+	AKI string `json:"aki,omitempty"`
+	Reason string `json:"reason,omitempty"`
+	CAName string `json:"caname,omitempty"`
+	// GenCRL specifies whether to generate a CRL. CRL will be returned only when AKI and Serial are provided.
+	GenCRL bool `json:"gencrl,omitempty"`
+}
+
+// CaRevokeResultCertificate identify revoked certificate
+type CaRevokeResultCertificate struct {
+	Serial string `json:"Serial"`
+	AKI string `json:"AKI"`
+}
+
+type CARevokeResult struct {
+	RevokedCertificates []CaRevokeResultCertificate `json:"RevokedCerts"`
+	CRL string `json:"CRL"`
+}
+
+type caRevokeResponse struct {
+	caResponse
+	Result CARevokeResult
+}
+
+type CaIdentityResponse struct {
+	ID             string                `json:"id"`
+	Type           string                `json:"type"`
+	Affiliation    string                `json:"affiliation"`
+	Attributes     []CaRegisterAttribute `json:"attrs" mapstructure:"attrs"`
+	MaxEnrollments int                   `json:"max_enrollments" mapstructure:"max_enrollments"`
+}
+
+type CAGetIdentityResponse struct {
+	CaIdentityResponse
+	CAName string `json:"caname"`
+}
+
+type caGetIdentity struct {
+	caResponse
+	Result CAGetIdentityResponse `json:"result"`
+}
+
+type CAListAllIdentitesResponse struct {
+	CAName     string               `json:"caname"`
+	Identities []CaIdentityResponse `json:"identities,omitempty"`
+}
+
+type caListAllIdentities struct {
+	caResponse
+	Result CAListAllIdentitesResponse `json:"result"`
 }
